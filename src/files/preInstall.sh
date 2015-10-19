e52c9547cb12c0455b4c7da04f5a205fe7d4c3bf
wdschei <Bill.Scheidegger@Rackspace.com>
2015-10-19 14:37:48 -0500

    Updated the Pre-Install adduser to check to see if it is already installed and successfully exit no matter what.

--- src/files/preInstall.sh
+++ src/files/preInstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Create the phone-home user to run the stuff
-adduser --system --no-create-home --group phone-home
+# Safely add the user/group.
+getent passwd phone-home >> /dev/null 2>&1 || adduser --system --no-create-home --group phone-home
+exit 0
